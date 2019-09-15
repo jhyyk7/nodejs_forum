9c6ddf496913607bb4a9fba05f1b60e835a7fbc5
jhyyk7 <jhyyk7@gmail.com>
2019-09-15 15:24:37 +0900

    Add readdir,readFile,querystring [main.js]

--- main.js
+++ main.js
@@ -1,16 +1,58 @@
 var http = require('http');
+var url = require('url');
+var fs = require ('fs');
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = 'hello world';
-    }
-    else if(request.url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end('Not found');
-        return;
-    }
-    response.writeHead(200);
-    response.end(url);
- 
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var title = queryData.id ;
+        if(_url == '/'){
+            var title = 'welcome';
+        }
+        if (_url =='/favicon.ico') {
+            
+                response.writeHead(404);
+                response.end('Not found');
+                return;
+            
+        }
+        //for (var i = 0; i<list.length; i++)
+            
+            fs.readdir('./data',function (err, list){
+
+                console.log(list[0]);
+            });
+            fs.readFile(`data/${title}`, 'utf8', function (err, description) {   
+                var data = description;
+                var Template_HTML = ` 
+                <!DOCTYPE html>
+                <html>
+                <head>
+                    <title>web1</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    ${list}
+                    <h2>${title}</h2>
+                    <p>${data}<p>
+                </body>
+                </html>
+                    `;
+                    
+            response.writeHead(200);
+            response.end(Template_HTML);
+            });
+        
+        
+    // <ul> 
+    //         <li><a href="/?id=HTML">HTML</a></li>
+    //         <li><a href="/?id=CSS">CSS</a></li>
+    //         <li><a href="/?id=Javascript">Javascript</a></li>
+    // </ul>
+        
+        
+        
+    
 });
 app.listen(3000);
