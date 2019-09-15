35a1a31116c53998c6aa05c0839c64af7b5a2cd9
jhyyk7 <jhyyk7@gmail.com>
2019-09-16 05:50:57 +0900

    Add readdir, No async/await [main.js]

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ var http = require('http');
 var url = require('url');
 var fs = require ('fs');
 
+
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var title = queryData.id ;
         if(_url == '/'){
             var title = 'welcome';
+            queryData.id ='WEB';
         }
-        if (_url =='/favicon.ico') {
+        else if (_url =='/favicon.ico') {
             
                 response.writeHead(404);
                 response.end('Not found');
@@ -17,32 +20,46 @@ var app = http.createServer(function(request,response){
             
         }
         //for (var i = 0; i<list.length; i++)
+        
             
-            fs.readdir('./data',function (err, list){
-
-                console.log(list[0]);
-            });
-            fs.readFile(`data/${title}`, 'utf8', function (err, description) {   
-                var data = description;
-                var Template_HTML = ` 
-                <!DOCTYPE html>
-                <html>
-                <head>
-                    <title>web1</title>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${list}
-                    <h2>${title}</h2>
-                    <p>${data}<p>
-                </body>
-                </html>
-                    `;
+        fs.readdir('./data',function(err, filelist) {
+            
+            var line = `<ul>`;
+            for (var i = 0; i <filelist.length; i++) {
+        
+                
+                 line = line + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            }
+                 line = line + `</ul>`;
+                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {   
                     
-            response.writeHead(200);
-            response.end(Template_HTML);
+                    var Template_HTML = ` 
+                    <!DOCTYPE html>
+                    <html>
+                    <head>
+                        <title>web1</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        ${line}
+                        <h2>${title}</h2>
+                        <p>${description}<p>
+                    </body>
+                    </html>
+                    `;
+                    response.writeHead(200);
+                    response.end(Template_HTML);
+                    console.log (url.parse(_url, true));
+                    console.log(line);
+            
+            
             });
+        
+        });
+            
+                    
+            
         
         
     // <ul> 
