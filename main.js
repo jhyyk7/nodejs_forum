02c492cf4cc6eceac174c30a41481c48100fa987
jhyyk7 <jhyyk7@gmail.com>
2019-09-16 21:34:52 +0900

    before main~template js [main,template.js]

--- main.js
+++ main.js
@@ -1,16 +1,54 @@
 var http = require('http');
 var url = require('url');
 var fs = require ('fs');
+var tmplate = require ('./template.js');
 
 
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var path_name = url.parse(_url, true).pathname;
     var title = queryData.id ;
-        if(_url == '/'){
-            var title = 'welcome';
-            queryData.id ='WEB';
+        if(path_name == '/'){
+            if(_url == '/')
+            {
+                var title = 'welcome';
+                queryData.id ='WEB';
+            }
+            
+            fs.readdir('./data',function(err, filelist) {
+            
+                var line = `<ul>`;
+                for (var i = 0; i <filelist.length; i++) {
+            
+                    
+                     line = line + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                }
+                     line = line + `</ul>`;
+                    fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {   
+                        
+                        var Template_HTML = ` 
+                        <!DOCTYPE html>
+                        <html>
+                        <head>
+                            <title>${title}</title>
+                            <meta charset="utf-8">
+                        </head>
+                        <body>
+                            <h1><a href="/">WEB</a></h1>
+                            ${line}
+                            <h2>${title}</h2>
+                            <p>${description}<p>
+                        </body>
+                        </html>
+                        `;
+                        response.writeHead(200);
+                        response.end(Template_HTML);
+                        console.log (url.parse(_url, true));
+                        console.log(line);
+                });
+            });
         }
         else if (_url =='/favicon.ico') {
             
@@ -19,57 +57,6 @@ var app = http.createServer(function(request,response){
                 return;
             
         }
-        //for (var i = 0; i<list.length; i++)
-        
             
-        fs.readdir('./data',function(err, filelist) {
-            
-            var line = `<ul>`;
-            for (var i = 0; i <filelist.length; i++) {
-        
-                
-                 line = line + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            }
-                 line = line + `</ul>`;
-                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {   
-                    
-                    var Template_HTML = ` 
-                    <!DOCTYPE html>
-                    <html>
-                    <head>
-                        <title>web1</title>
-                        <meta charset="utf-8">
-                    </head>
-                    <body>
-                        <h1><a href="/">WEB</a></h1>
-                        ${line}
-                        <h2>${title}</h2>
-                        <p>${description}<p>
-                    </body>
-                    </html>
-                    `;
-                    response.writeHead(200);
-                    response.end(Template_HTML);
-                    console.log (url.parse(_url, true));
-                    console.log(line);
-            
-            
-            });
-        
-        });
-            
-                    
-            
-        
-        
-    // <ul> 
-    //         <li><a href="/?id=HTML">HTML</a></li>
-    //         <li><a href="/?id=CSS">CSS</a></li>
-    //         <li><a href="/?id=Javascript">Javascript</a></li>
-    // </ul>
-        
-        
-        
-    
 });
 app.listen(3000);
---
+++ tepmlate.js
@@ -0,0 +1,84 @@
+var fs = require ('fs');
+var read = function () {
+    return new Promise(function(resolve, reject) {
+        fs.readFile('./sample.txt', 'utf8', function(err, result){
+            if(err) reject(err);
+            else {
+                
+                console.log("읽기 : " + result);
+                resolve();
+            }
+        });
+    });
+};
+
+var write = function (buf){
+    return new Promise(function(resolve, reject) {
+        fs.writeFile('./sample.txt', buf, function(err) {
+            if (err) reject(err);
+            else {
+                console.log('쓰기 완료');
+                resolve();
+            }
+        });
+    });
+};
+
+var readdir = function (id) {
+    return new Promise(function (resolve, reject) {
+        fs.readFile (`../data/${id}`,'utf8',function(err, description) {
+                if (err) reject(err);
+                else {
+                    console.log(description);
+                    resolve();
+                }
+        })
+    })
+}
+
+async function testD() {
+
+    var buf = 'hello';
+    await write(buf); 
+    
+    
+}
+async function testB() {
+
+    
+    var id = 'CSS';
+    await readdir(id);
+    
+}
+async function testC() {
+
+    
+    
+    await read();
+    
+}
+
+module.exports = { //TODO: 각 함수들 다시한번 정리하고 nodejs_forum/main.js 에 apply 해보기.
+      fs_ : async function testD() {
+
+        var buf = 'hello';
+        await write(buf); 
+        
+        
+    }
+    , a2 : async function testB() {
+
+    
+        var id = 'CSS';
+        await readdir(id);
+        
+    }
+    , a3 : async function testC() {
+
+    
+    
+        await read();
+        
+    }
+}
+
